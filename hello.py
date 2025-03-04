203664c94fd355614ba74130788efc72cb5dcbfc
Griffin Wang <468791220@qq.com>
2025-03-05 00:05:40 +0800

    test

---
+++ hello.py
@@ -0,0 +1,26 @@
+# from scipy.optimize import fsolve
+# import numpy as np
+
+# # 定义非线性方程组
+# def equations(vars):
+#     x, y = vars
+#     eq1 = x + y**2 - 2
+#     eq2 = x**2 + y - 1
+#     return [eq1, eq2]
+
+# # 初始猜测值
+# initial_guess = [1, 1]
+
+# # 求解方程组
+# solution = fsolve(equations, initial_guess)
+
+# # 输出解
+# x = solution[0]
+# y = solution[1]
+# print(f"x 的值为: {x}")
+# print(f"y 的值为: {y}")
+
+A = 1
+B = 2
+C = 3
+print(A)
